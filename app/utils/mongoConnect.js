743486ebee43c0fd9ab59526e3103bddddfdd546
de-code-ninja <166255748+de-code-ninja@users.noreply.github.com>
2024-05-09 02:01:56 +0500

    Update mongoConnect.js

--- app/utils/mongoConnect.js
+++ app/utils/mongoConnect.js
@@ -1,7 +1,7 @@
 import mongoose from "mongoose";
 
 async function mongoConnect(){
-   (await mongoose.connect("mongodb://localhost:27017/urlShortner")).isObjectIdOrHexString(res=>{
+   (await mongoose.connect(process.env.MONGO_URI)).isObjectIdOrHexString(res=>{
       console.log("database connected");
    })
 }
